4672e6a9f78d3f259f854c87e3b45563a2df321e
prakash chaudhary <prakashkchaudhary5209@gmail.com>
2023-06-30 22:55:13 +0545

    stack

--- stack.c
+++ stack.c
@@ -1,56 +1,63 @@
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
+#define MAXSIZE 4
 
-#define SIZE 4
+int top = -1,item,stack[MAXSIZE];
 
-int top = 0,arry[SIZE];
 
+void insertItem();
+void showItem();
+void popItem();
 int main(){
-    int n;
-    int item;
+    printf("1 insert the item into stack\n2 show all element\n3 deletion the element from stack\n4 terminate\n");
     while(1){
-        printf("1 insert the item into stack\n2 deletion the element from stack\n3 show all element\n4 terminate\n");
-        printf(": ");
-        scanf("%d",&n); 
-        switch(n){
+        printf("enter the operation: ");
+        scanf("%d",&item);
+        switch(item){
             case 1:
-                if(top == SIZE){
-                    printf("stack is full\n");
-                }else{
-                    printf("enter a element:");
-                    scanf("%d",&item);
-                    arry[top] = item;
-                    top += 1;
-                }
+                insertItem();
                 break;
             case 2:
-                if(top == 0){
-                    printf("stack is empty\n");
-                }else{
-                    printf("poped item: %d",arry[top-1]);
-                    top = top - 1;
-                }
+                showItem();
                 break;
-
             case 3:
-                if(top == 0){
-                    printf("stack is empty\n");
-                }else{
-                    for(int i = top - 1;i >= 0;i--){
-                        printf("%d\n",arry[i]);
-                    }
-                }
+                popItem();
                 break;
-
             case 4:
+                printf("program is terminated\n");
                 exit(0);
-
-            default:
-                printf("plz enter the corrrect opeartion\n");
-                break;
-            }
-        printf("\n");
+        }
     }
     return 0;
+}
+
+void insertItem(){
+    if(top == MAXSIZE-1){
+        printf("stack is full\n");
+    }else{
+        printf("enter a item to insert in stack: ");
+        scanf("%d",&item);
+        top += 1;
+        stack[top] = item;
+    }
+}
+
+void showItem(){
+    if(top == -1){
+        printf("stack is empty\n");
+    }else{
+        for(int i = 0;i <= top;i++){
+            printf("%d\n",stack[i]);
+        }
+    }
+}
+
+void popItem(){
+    if(top == -1){
+        printf("nothing to pop\n");
+    }else{
+        top = top - 1;
+        printf("poped item: %d\n",stack[top + 1]);
+    }
 }
